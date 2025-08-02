8f7702c1f16ddbd57c228e9d619f53c805978811
Azza <azzashahen2004@gmail.com>
2025-08-02 16:34:44 +0300

    Complete Book model and configure many-to-many relationship with Category through BookCategory

---
+++ Bookify.Web/Core/Models/BookCategory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Bookify.Web.Core.Models
+{
+    public class BookCategory
+    {
+        //many-to-many Relationship between books and categories
+        //Foreign Key(Composite Key)=>this means that every combination of Book and Category will be unique and will not be repeated.
+        public int BookId { get; set; }
+        public int CategoryId { get; set; }
+
+        // Navigation Properties
+        public Book? Book { get; set; }
+        public Category? Category { get; set; }
+    }
+}
--- Bookify.Web/Core/Models/Category.cs
+++ Bookify.Web/Core/Models/Category.cs
@@ -16,5 +16,8 @@
         [MaxLength(100)]
         public string Name { get; set; } = null!;
 
+        public ICollection<BookCategory> Books { get; set; } = new HashSet<BookCategory>();
+
+
     }
 }
